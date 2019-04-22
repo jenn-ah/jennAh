0f1b0dd9f502c922ffbb7379c7c07fdae6f7e0ab
jenn-ah <jahuna@live.com>
2019-04-22 12:35:32 -1000

    reconfigured docker setup, updates accordingly

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
-require('dotenv').config();
 
 const express = require('express');
 const app = express();
-const PORT = process.env.EXPRESS_CONTAINER_PORT;
+const PORT = process.env.EXPRESS_CONTAINER_PORT || 8989;
 const bodyParser = require('body-parser');
 
+
+
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/smoke', () => {
-  res.send('smoke test');
-})
+app.get('/smoke', (req, res) => {
+  res.send('jenn');
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
 });
+
